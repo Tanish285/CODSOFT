ef9bdd3ef573dfbdf3a4975a5c66a5827b0e17a3
Tanish285 <101703389+Tanish285@users.noreply.github.com>
2024-03-08 23:17:57 +0530

    Add files via upload

---
+++ TASK_1_bloggingplatform/app.py.py
@@ -0,0 +1,27 @@
+import datetime
+from flask import Flask,render_template
+
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+# @app.route('/')
+# def bootstrap():
+#     return render_template('bootstap.html')
+
+
+
+# if __name__=='__main__':
+app.run(debug=True)
+
+    
